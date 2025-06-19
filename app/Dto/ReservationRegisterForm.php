c300aee5fd3a6d6b2cab28077b0da583cec38f1c
VD3baeKy <digfehc@gmail.com>
2025-06-20 00:46:39 +0900

    Create ReservationRegisterForm.php

---
+++ app/Dto/ReservationRegisterForm.php
@@ -0,0 +1,22 @@
+<?php
+
+  namespace app\Dto;
+
+  class ReservationRegisterForm{
+    public $houseId;
+    public $userId;
+    public $checkinDate;
+    public $checkoutDate;
+    public $numberOfPeople;
+    public $amount;
+
+    // コンストラクタで全フィールド初期化
+    public function __construct($houseId, $userId, $checkinDate, $checkoutDate, $numberOfPeople, $amount){
+        $this->houseId = $houseId;
+        $this->userId = $userId;
+        $this->checkinDate = $checkinDate;
+        $this->checkoutDate = $checkoutDate;
+        $this->numberOfPeople = $numberOfPeople;
+        $this->amount = $amount;
+    }
+  }
