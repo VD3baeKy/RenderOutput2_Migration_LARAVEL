759acb2e7a53c2a0c94211a535b9373abcb163f0
VD3baeKy <digfehc@gmail.com>
2025-06-20 02:50:31 +0900

    Create styles.blade.php

---
+++ resources/views/fragment/styles.blade.php
@@ -0,0 +1,9 @@
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">
+
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
