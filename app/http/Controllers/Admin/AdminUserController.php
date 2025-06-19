96bb81b394ace78cbe3247d987aa04ffc1fab2c9
VD3baeKy <digfehc@gmail.com>
2025-06-19 23:38:56 +0900

    Update AdminUserController.php

--- app/http/Controllers/Admin/AdminUserController.php
+++ app/http/Controllers/Admin/AdminUserController.php
@@ -1,38 +1,35 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+    namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Pagination\Paginator;
+    use App\Http\Controllers\Controller;
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Illuminate\Pagination\Paginator;
 
-class AdminUserController extends Controller
-{
-    public function index(Request $request)
-    {
-        $keyword = $request->input('keyword');
-        $query = User::query();
+    class AdminUserController extends Controller{
+        public function index(Request $request){
+            $keyword = $request->input('keyword');
+            $query = User::query();
 
-        if ($keyword) {
-            $query->where('name', 'like', '%' . $keyword . '%')
-                  ->orWhere('furigana', 'like', '%' . $keyword . '%');
+            if ($keyword) {
+                $query->where('name', 'like', '%' . $keyword . '%')
+                      ->orWhere('furigana', 'like', '%' . $keyword . '%');
+            }
+
+            $users = $query->orderBy('id', 'asc')->paginate(10);
+
+            return view('admin.users.index', [
+                'userPage' => $users,
+                'keyword' => $keyword,
+            ]);
         }
 
-        $users = $query->orderBy('id', 'asc')->paginate(10);
+        public function show($id){
+            $user = User::findOrFail($id);
 
-        return view('admin.users.index', [
-            'userPage' => $users,
-            'keyword' => $keyword,
-        ]);
+            return view('admin.users.show', [
+                'user' => $user,
+            ]);
+        }
     }
-
-    public function show($id)
-    {
-        $user = User::findOrFail($id);
-
-        return view('admin.users.show', [
-            'user' => $user,
-        ]);
-    }
-}
