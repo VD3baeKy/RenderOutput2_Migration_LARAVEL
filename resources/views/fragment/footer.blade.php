80cf63fa2f940a62a46239343fe9348402cdddf0
VD3baeKy <digfehc@gmail.com>
2025-06-20 06:30:37 +0900

    Create footer.blade.php

---
+++ resources/views/fragment/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto py-3 bg-light text-center">
+    <div class="container">
+        <span class="text-muted">&copy; {{ date('Y') }} Samurai Travel</span>
+    </div>
+</footer>
