3d29994f867e7b1af8fc879ffd5ef54b1745e09b
VD3baeKy <digfehc@gmail.com>
2025-06-20 00:18:49 +0900

    Create ReviewDto.php

---
+++ app/http/Requests/ReviewDto.php
@@ -0,0 +1,31 @@
+<?php
+// app/Http/Requests/ReviewDto.php
+  namespace app\http\Requests;
+
+  use Illuminate\Foundation\Http\FormRequest;
+
+  class ReviewDto extends FormRequest{
+    public function authorize()
+    {
+        return true;
+    }
+
+    // バリデーションルール
+    public function rules()
+    {
+        return [
+            'content' => 'required|string|max:1000',
+            'rating'  => 'required|integer|min:1|max:5'
+        ];
+    }
+
+    // ゲッターの代わりにこう使える
+    public function getContent()
+    {
+        return $this->input('content');
+    }
+    public function getRating()
+    {
+        return $this->input('rating');
+    }
+  }
