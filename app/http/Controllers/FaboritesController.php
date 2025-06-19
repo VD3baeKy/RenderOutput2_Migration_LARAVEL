870d3840305ea7bd2c84a2a7487d1af6e27b6b5a
VD3baeKy <digfehc@gmail.com>
2025-06-19 23:26:30 +0900

    Create FaboritesController.php

---
+++ app/http/Controllers/FaboritesController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\http\Controllers;
+
+use app\Models\User;
+use app\Models\Loves;
+use app\Models\House;
+use app\Services\ReservationService;
+use app\http\DTO\FaboriteHouseDTO;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class FaboritesController extends Controller
+{
+    protected $lovesRepository;
+    protected $houseRepository;
+    protected $reservationService;
+
+    public function __construct(LovesRepository $lovesRepository, HouseRepository $houseRepository, ReservationService $reservationService)
+    {
+        $this->lovesRepository = $lovesRepository;
+        $this->houseRepository = $houseRepository;
+        $this->reservationService = $reservationService;
+    }
+
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+        $page = $request->input('page', 1);
+        $perPage = 5;
+
+        // ユーザーのお気に入りを取得
+        $fabos = $this->lovesRepository->getFabos($user->id, $page, $perPage);
+        
+        // DTOの作成
+        $faboriteHouseDTOs = [];
+        foreach ($fabos as $loves) {
+            $house = $this->houseRepository->find($loves->houseid);
+            $faboriteHouseDTOs[] = new FaboriteHouseDTO($loves, $house);
+        }
+
+        // ページネーションの設定
+        $fabosPage = new LengthAwarePaginator($faboriteHouseDTOs, $fabos->total(), $perPage, $page);
+
+        return view('faborites.index', ['fabosPage' => $fabosPage]);
+    }
+}
